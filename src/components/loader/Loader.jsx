200e18798a612adc2822decb44f49b27839badd3
Demag0g1 <konievanatol@gmail.com>
2023-05-01 01:24:40 +0300

    update loading

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,2 @@
 
 
-  <Triangle
-  height="80"
-  width="80"
-  color="#4fa94d"
-  ariaLabel="triangle-loading"
-  wrapperStyle={{}}
-  wrapperClassName=""
-  visible={true}
-/>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ImageGallery from '../imageGallery/ImageGallery';
+import {Blocks} from 'react-loader-spinner';
 import styles from './SearchBar.module.css';
 
 class SearchBar extends Component {
@@ -10,6 +11,7 @@ class SearchBar extends Component {
     apiURL: 'https://pixabay.com/api',
     apiKey: '35920298-91199a46b82f570ed53995cb2',
     images: [],
+    loading: false,
   };
 
   onInputChange = e => {
@@ -19,44 +21,61 @@ class SearchBar extends Component {
   onFormSubmit = e => {
     e.preventDefault();
     const { inputText, amount, apiURL, apiKey } = this.state;
+    this.setState({ loading: true });
 
     axios
       .get(
         `${apiURL}/?q=${inputText}&page=1&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${amount}&safesearch=true`
       )
       .then(res => {
-        this.setState({ images: res.data.hits });
+        this.setState({ images: res.data.hits, loading: false });
       })
       .catch(err => console.log(err));
   };
 
   render() {
-    const { inputText, images } = this.state;
+    const { inputText, images, loading } = this.state;
 
     return (
       <>
-      <header className={styles.SearchBar}>
-        <form className={styles.SearchForm} onSubmit={this.onFormSubmit}>
-          <button className={styles['SearchForm-button']} type="submit">
-            <span className={styles['SearchForm-button-label']}>Search</span>
-          </button>
-          <input
-            className={styles['SearchForm-input']}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={inputText}
-            onChange={this.onInputChange}
-          />
-        </form>
-      </header>
-     <ImageGallery images={images} />
-     </>
+        <header className={styles.SearchBar}>
+          <form className={styles.SearchForm} onSubmit={this.onFormSubmit}>
+            <button className={styles['SearchForm-button']} type="submit">
+              <span className={styles['SearchForm-button-label']}>Search</span>
+            </button>
+            <input
+              className={styles['SearchForm-input']}
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              value={inputText}
+              onChange={this.onInputChange}
+            />
+          </form>
+        </header>
+        {loading ? (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '350px',
+            }}
+          >
+            <Blocks
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="blocks-loading"
+              wrapperClass="blocks-wrapper"
+            />
+          </div>
+        ) : (
+          <ImageGallery images={images} />
+        )}
+      </>
     );
-    
   }
-  ;
-};
+}
 
 export default SearchBar;
