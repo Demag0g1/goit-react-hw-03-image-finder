f99fc13fea1c87219b3af96d2778ec7aa34ed792
Demag0g1 <konievanatol@gmail.com>
2023-05-01 01:56:15 +0300

    up modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import SearchBar from './searchBar/SearchBar';
-// import ImageGalleryItem from './imageGalleryItem/ImageGalleryItem';
-
 
 const App = () => {
   return (
     <div>
-     <SearchBar />
-   
+      <SearchBar />
     </div>
   );
 };
